c5a96a69fe11d3d3decd501c9875186c61080a6c
Ryanryanjp <kidamaru@gmail.com>
2017-01-30 21:06:29 +0900

    ショートカットキー実装しました
    ファイルメニューやらステータスバーやらいろいろつきましたが
    ショートカットキー実装するのに必要だったのでつけました
    めっちゃ大変でしたね...

--- qt-tweepy.py
+++ qt-tweepy.py
@@ -1,81 +1,136 @@
-#! /usr/bin/python3
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# PyQt5でtweepy_tkinterを実装します
-# Forkしないてすと
+# クラスが増えた
 
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton,
+from PyQt5.QtWidgets import (QMainWindow, QAction, qApp,
+                             QApplication, QWidget, QPushButton,
                              QHBoxLayout, QVBoxLayout, QLabel,
                              QLineEdit)
 from PyQt5.QtCore import Qt
 from tweet_api import*
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
+class Example(QMainWindow):
 
-    window = QWidget()
+    #おまじないみたいなもの
+    #呼びだされたらこんなかのものが実行されると考えていい
+    def __init__(self):
+        super().__init__()
 
-    tw_api = get_twitter_api()
+        self.initUI()
 
-    def getTweet():
-        label = QLabel(tw_api.get_tl(), window)
+    def initUI(self):
+
+        # アクションオブジェクト作成
+        getAction   = QAction('Get Tweet', self)
+        tweetAction = QAction('Post Tweet', self)
+        exitAction  = QAction('Exit', self)
+
+        # ショートカットの設定
+        getAction.setShortcut('Ctrl+R')
+        tweetAction.setShortcut('Ctrl+Return')
+        exitAction.setShortcut('Ctrl+Q')
+
+        # カーソルを乗せるとステータスバーへ表示
+        getAction.setStatusTip('Tweetを取得')
+        tweetAction.setStatusTip('Tweetを送信')
+        exitAction.setStatusTip('アプリケーションを終了')
+
+        # スロットの設定
+        getAction.triggered.connect(win.getTweet)
+        tweetAction.triggered.connect(win.post_tweet)
+        exitAction.triggered.connect(app.quit)
+
+        # ステータスバーの設定
+        self.statusBar()
+
+        # メニューバー作成
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('ファイル')
+
+        # Actionを紐づける
+        fileMenu.addAction(getAction)
+        fileMenu.addAction(tweetAction)
+        fileMenu.addAction(exitAction)
+
+        # Windowクラスを中央に設置
+        self.setCentralWidget(win)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('下半身がグッドゲーム')
+        self.show()
+
+class Window(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.parts()
+
+    def getTweet(self):
+        label = QLabel(tw_api.get_tl(), self)
         label.setAlignment(Qt.AlignBottom | Qt.AlignHCenter)
-        Ylayout.addWidget(label)
+        self.Ylayout.addWidget(label)
 
-    def post_tweet():
+    def post_tweet(self):
         try:
-            get_value = twinput.text()
+            get_value = self.twinput.text()
 
             if(len(get_value) > 140):
                 strCnt = '文字数が140を超えているのでTweetできません'
-                label = QLabel(strCnt, window)
+                label = QLabel(strCnt, self)
                 label.setAlignment(Qt.AlignBottom | Qt.AlignHCenter)
-                Ylayout.addWidget(label)
+                self.Ylayout.addWidget(label)
 
             else:
                 tw_api.post(get_value)
-                twinput.clear()
+                self.twinput.clear()
 
         except Exception as err:
             errMess = 'Tweetエラー:{0}'.format(err.args[0][0]['message'])
-            label = QLabel(errMess, window)
+            label = QLabel(errMess, self)
             label.setAlignment(Qt.AlignBottom | Qt.AlignHCenter)
-            Ylayout.addWidget(label)
+            self.Ylayout.addWidget(label)
 
+    def parts(self):
+        # ボタンを作成
+        hi_there = QPushButton('Get Tweet', self)
+        end      = QPushButton('QUIT', self)
+        send     = QPushButton('Post Tweet', self)
 
-    # ボタンを作成
-    hi_there = QPushButton('Get Tweet', window)
-    end      = QPushButton('QUIT', window)
-    send     = QPushButton('Post Tweet', window)
+        # 入力フォームの作成
+        self.twinput  = QLineEdit(self)
 
-    # 入力フォームの作成
-    twinput  = QLineEdit(window)
+        # ボタンのスタイルシートを設定
+        end.setStyleSheet('color:red')
 
-    # ボタンのスタイルシートを設定
-    end.setStyleSheet('color:red')
+        # スロットを設定
+        hi_there.clicked.connect(self.getTweet)
+        end.clicked.connect(app.quit)
+        send.clicked.connect(self.post_tweet)
 
-    # スロットを設定
-    hi_there.clicked.connect(getTweet)
-    end.clicked.connect(app.quit)
-    send.clicked.connect(post_tweet)
+        # レイアウト作成
+        Xlayout1 = QHBoxLayout()
+        Xlayout2 = QHBoxLayout()
+        self.Ylayout  = QVBoxLayout()
 
-    # レイアウト作成
-    Xlayout1 = QHBoxLayout()
-    Xlayout2 = QHBoxLayout()
-    Ylayout  = QVBoxLayout()
+        # レイアウトにボタンを追加
+        Xlayout1.addWidget(self.twinput)
+        Xlayout1.addWidget(send)
+        Xlayout2.addWidget(hi_there)
+        Xlayout2.addWidget(end)
 
-    # レイアウトにボタンを追加
-    Xlayout1.addWidget(twinput)
-    Xlayout1.addWidget(send)
-    Xlayout2.addWidget(hi_there)
-    Xlayout2.addWidget(end)
+        # 縦レイアウトに横レイアウト*2を追加
+        self.Ylayout.addLayout(Xlayout1)
+        self.Ylayout.addLayout(Xlayout2)
 
-    # 縦レイアウトに横レイアウト*2を追加
-    Ylayout.addLayout(Xlayout1)
-    Ylayout.addLayout(Xlayout2)
+        # レイアウトをセット
+        self.setLayout(self.Ylayout)
 
-    # レイアウトをセット
-    window.setLayout(Ylayout)
+if __name__ == '__main__':
 
-    window.show()
+    app = QApplication(sys.argv)
+    tw_api = get_twitter_api()
+    win = Window()
+    ex = Example()
     sys.exit(app.exec_())
